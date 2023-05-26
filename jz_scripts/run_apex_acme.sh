b923d0cc0703bd0ab3ea4d4a28918ce795f47f84
Eleni Nisioti <elennisioti@gmail.com>
2023-05-26 15:47:35 +0200

    library fiz

--- jz_scripts/run_apex_acme.sh
+++ jz_scripts/run_apex_acme.sh
@@ -5,5 +5,5 @@
 #SBATCH --ntasks-per-node=10
 #SBATCH --output=/scratch/enisioti/acme_log/jz_logs/%j.out
 #SBATCH --error=/scratch/enisioti/acme_log/jz_logs/%j.err
-
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs/home/enisioti/anaconda3/envs/acme/lib
 python examples/baselines/rl_discrete/run_dqn_custom.py
