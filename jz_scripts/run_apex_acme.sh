f0627e8d53b0c36e2bc848fb5084e84bc817c80c
Eleni Nisioti <elennisioti@gmail.com>
2023-05-30 16:15:12 +0200

    removed files

--- jz_scripts/run_apex_acme.sh
+++ jz_scripts/run_apex_acme.sh
@@ -2,6 +2,7 @@
 #SBATCH -J beamrdiderapexacme
 #SBATCH --cpus-per-task 64
 #SBATCH -t 24:00:00
+#SBATCH --ntasks-per-node=10
 #SBATCH --output=/scratch/enisioti/acme_log/jz_logs/%j.out
 #SBATCH --error=/scratch/enisioti/acme_log/jz_logs/%j.err
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs/home/enisioti/anaconda3/envs/acme/lib
