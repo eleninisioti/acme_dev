1bdce5e291b462e57d00df0ffed559c39adeb028
Eleni Nisioti <elennisioti@gmail.com>
2023-05-31 10:47:01 +0200

    fix

--- jz_scripts/run_apex_acme.sh
+++ jz_scripts/run_apex_acme.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH -J beamrdiderapexacme
+#SBATCH -J acmeapex
 #SBATCH --cpus-per-task 64
 #SBATCH -t 24:00:00
-#SBATCH --ntasks-per-node=10
 #SBATCH --output=/scratch/enisioti/acme_log/jz_logs/%j.out
 #SBATCH --error=/scratch/enisioti/acme_log/jz_logs/%j.err
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs/home/enisioti/anaconda3/envs/acme/lib
